2a439ff91115bd85906ad58301e68c765971f7bd
Kamva-Hanisi <lucashanisi@gmail.com>
2026-09-25 15:07:56 +0200

    Handle Aiven certificate on Render

--- backend/docker/start-render.sh
+++ backend/docker/start-render.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+if [ -f /etc/secrets/ca.pem ]; then
+    cp /etc/secrets/ca.pem /tmp/aiven-ca.pem
+    chmod 0644 /tmp/aiven-ca.pem
+fi
+
 php artisan config:cache
 php artisan route:cache
 php artisan migrate --force
